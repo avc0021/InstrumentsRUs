a345060dadfa3bb13413cb51365fe62f66769130
bhari eutsey <bharilynn@outlook.com>
2022-02-13 20:32:27 -0600

    user and instrument seeds

---
+++ models/Instruments.js
@@ -0,0 +1,43 @@
+
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+// create our User model
+class Instrument extends Model {}
+
+// create fields/columns for User model
+Instrument.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    instrument: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    brand_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+        validate: {
+          isDecimal: true
+        }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
+);
+
+module.exports = Instrument;
