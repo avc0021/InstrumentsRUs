ad11f1404be32ed76e16ee938563895ade1dbae7
Sodaman <mattrick2214@gmail.com>
2022-02-15 14:35:52 -0600

    attempting to get run seed to work

--- seeds/instrument-seeds.js
+++ seeds/instrument-seeds.js
@@ -5,14 +5,14 @@ const instrumentDetails = [
     instrument: 'Tenor Saxophone',
     brand_name: 'Yamaha Custom Z',
     price: 5100,
-    filename: 'tenorsax.png',
+    file_name: 'tenorsax.png',
     user_id: 1
   },
   {
     instrument: 'Banjo',
     brand_name: 'Epiphone MB-100 First Pick Banjo',
     price: 250,
-    filename: 'banjo.png',
+    file_name: 'banjo.png',
     user_id: 1
   },
 ];
