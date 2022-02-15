ba96704c4b7adf2605121d601c1fb5167b16314f
avc0021 <avc0021@gmail.com>
2022-02-14 19:19:02 -0600

    login js and login hb update

--- public/js/login.js
+++ public/js/login.js
@@ -27,15 +27,15 @@ async function loginFormHandler(event) {
 async function signupFormHander(event) {
     event.preventDefault();
 
-    const username = document.querySelector('#username-signup').value.trim();
+    //const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
-    if (username && email && password) {
-        const reponse = await fetch('/api/users', {
+    if ( email && password) {
+        const response = await fetch('/api/users', {
             method: 'post',
             body: JSON.stringify({
-                username,
+                // username,
                 email,
                 password
             }),
