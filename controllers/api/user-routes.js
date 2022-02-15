63f56d33cf27c232f9cc57324a8fb94b1269c814
avc0021 <avc0021@gmail.com>
2022-02-15 17:25:43 -0600

    login working

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -22,40 +22,31 @@ router.post('/', async (req, res) => {
 
 // For login
 router.post('/login', async (req, res) => {
-  try {
-    const dbUserData = await User.findOne({
-      where: {
-        email: req.body.username,
-      },
-    });
-
+  User.findOne({
+    where: {
+      username: req.body.username
+    }
+  }).then(dbUserData => {
     if (!dbUserData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect username or password. Please try again!' });
+      res.status(400).json({ message: 'No user found!' });
       return;
     }
 
-    const validPassword = await dbUserData.checkPassword(req.body.password);
+    const validPassword = dbUserData.checkPassword(req.body.password);
 
     if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect username or password. Please try again!' });
+      res.status(400).json({ message: 'Incorrect password!' });
       return;
     }
 
     req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
       req.session.loggedIn = true;
-
-      res
-        .status(200)
-        .json({ user: dbUserData });
+  
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
     });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+  });
 });
 
 // Logout
